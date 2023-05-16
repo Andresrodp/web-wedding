7a6621d25f88c0573d7f30ab3f011e7f2d92716a
Andresrodp <arodriguez@plm.com.co>
2023-05-16 18:03:52 -0500

    fotos definitivas y mejora en el inicio

--- client/src/pages/content/[id]/index.jsx
+++ client/src/pages/content/[id]/index.jsx
@@ -5,6 +5,11 @@ import SaltoLinea from "@/components/saltoLinea.jsx";
 import SaludoInicial from "@/components/saludoInicial";
 import InfoBoda from "@/components/infoBoda";
 import Regalos from "@/components/regalos"
+import slider1 from "../../../../assets/images/slider-1.jpg";
+import slider2 from "../../../../assets/images/slider-2.jpg";
+import slider3 from "../../../../assets/images/slider-3.jpg";
+import slider4 from "../../../../assets/images/slider-4.jpg";
+import slider5 from "../../../../assets/images/slider-5.jpg";
 
 const content = () => {
   const router = useRouter()
@@ -17,18 +22,21 @@ const content = () => {
       <div className="h-24 w-full">
         <Image src={img} className="h-full w-full object-cover" alt="titulo" />
       </div>
-      <div className="snap-mandatory snap-x h-2/6 w-full flex items-center justify-start gap-1 overflow-scroll xl:flex xl:h-4/5 xl:w-full xl:gap-2 xl:overflow-scroll xl:items-start">
+      <div className="snap-mandatory snap-x h-4/6 w-full flex items-center justify-start gap-1 overflow-scroll xl:flex xl:h-4/5 xl:w-full xl:gap-2 xl:overflow-scroll xl:items-start">
         <div className="bg-cover w-4/5 h-full shrink-0 snap-center rounded-md xl:w-4/5 xl:h-3/5 xl:aspect-video">
-          <img className="aspect-video object-cover rounded-md xl:object-cover xl:aspect-video " src="https://static.nationalgeographic.es/files/styles/image_3200/public/panda%2001.webp?w=710&h=1065" alt="" />
+          <Image className="aspect-video object-cover rounded-md xl:object-cover xl:aspect-video " src={slider1} alt="" />
         </div>
         <div className="w-4/5 h-full shrink-0 snap-center bg-cover rounded-md">
-          <img className="aspect-video object-cover rounded-md xl:aspect-video" src="https://static.nationalgeographic.es/files/styles/image_3200/public/Panda%2002.webp?w=1190&h=792&q=100" alt="" />
+          <Image className="aspect-video object-cover object-top rounded-md xl:aspect-video" src={slider2} alt="" />
         </div>
         <div className="bg-cover w-4/5 h-full shrink-0 snap-center rounded-md">
-          <img className="aspect-video object-cover rounded-md xl:aspect-video" src="https://static.nationalgeographic.es/files/styles/image_3200/public/panda%2001.webp?w=710&h=1065" alt="" />
+          <Image className="aspect-video object-cover object-top rounded-md xl:aspect-video" src={slider3} alt="" />
         </div>
         <div className="bg-cover w-4/5 h-full shrink-0 snap-center rounded-md">
-          <img className="aspect-video object-cover rounded-md xl:aspect-video" src="https://static.nationalgeographic.es/files/styles/image_3200/public/Panda%2002.webp?w=1190&h=792&q=100" alt="" />
+          <Image className="aspect-video object-cover rounded-md xl:aspect-video" src={slider4} alt="" />
+        </div>
+        <div className="bg-cover w-4/5 h-full shrink-0 snap-center rounded-md">
+          <Image className="aspect-video object-cover rounded-md xl:aspect-video" src={slider5} alt="" />
         </div>
       </div>
       <SaltoLinea />
